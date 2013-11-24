2e10e35f5108bc4071917bb7e06ba977f66b60c1
Adam Albrecht <adam.albrecht@gmail.com>
2013-11-23 20:06:29 -0800

    Minified version of JS and some re-organization

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,30 @@ module.exports = function(grunt) {
     coffee: {
       compile: {
         files: {
-          "build/spec/specs.js": ["spec/*.coffee"],
-          "build/src/ng-quick-date.js": ["src/*.coffee"]
+          "spec/build/specs.js": ["spec/*.coffee"],
+          "build/ng-quick-date.js": ["src/*.coffee"]
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          "build/ng-quick-date.min.js": "build/ng-quick-date.js"
         }
       }
     },
     stylus: {
       compile: {
         files: {
-          "build/src/ng-quick-date.css": ["src/ng-quick-date.styl"],
-          "build/src/ng-quick-date-default-theme.css": ["src/ng-quick-date-default-theme.styl"]
+          "build/ng-quick-date.css": ["src/ng-quick-date.styl"],
+          "build/ng-quick-date-default-theme.css": ["src/ng-quick-date-default-theme.styl"]
         }
       }
     },
     watch: {
       scripts: {
         files: ['**/*.coffee', '**/*.styl'],
-        tasks: ['coffee', 'stylus'],
+        tasks: ['coffee', 'uglify', 'stylus'],
         options: {
           debounceDelay: 250,
         },
@@ -31,8 +38,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['coffee', 'stylus', 'watch']);
+  grunt.registerTask('default', ['coffee', 'uglify', 'stylus', 'watch']);
 };

0e15e9dc3159e5c5c3946d5f60a4c92b9fa87000
Andrew Coldham <quasor@me.com>
2014-03-10 14:59:10 -0700

    only update scope hours if set

--- dist/fd-quick-moment.js
+++ dist/fd-quick-moment.js
@@ -235,7 +235,7 @@
             if (typeof scope.dateFilter === 'function' && !scope.dateFilter(date)) {
               return false;
             }
-            if (scope.disableTimepicker) {
+            if (scope.disableTimepicker && scope.ngModel !== undefined) {
               hours = scope.ngModel.hours();
               minutes = scope.ngModel.minutes();
               date = date.hours(hours).minutes(minutes);
